700ade1d412d71728f2c04c95bf1a78eb4284411
Priyank Agarwal <priyankagarwal00.pa@gmail.com>
2020-06-26 21:58:24 +0530

    Image Gradient

--- Day5/image_gradients.py
+++ Day5/image_gradients.py
@@ -10,11 +10,24 @@ while cap.isOpened():
 
 # LAPLACIAN 2nd argument is datatype 64 float , 3rd argument is kernel size  , it is optional
 	lap = cv2.Laplacian(gray , cv2.CV_64F , ksize=5)
+	
+# SobelX 2nd arg is datatype 64 float 3rd is value of dx and 4th is dy
+	sobelX = cv2.Sobel(gray , cv2.CV_64F , 1 ,0)
+	sobelX = np.uint8(np.absolute(sobelX))
 
+# SObelY
+	sobelY = cv2.Sobel(gray , cv2.CV_64F , 0 ,1)
+	sobelY = np.uint8(np.absolute(sobelY))
+
+# Sobel Combined
+	sobel_com = cv2.bitwise_or(sobelX ,sobelY)
 # Converting into Uint8
 	lap = np.uint8(np.absolute(lap))
 		
 	cv2.imshow('laplacian', lap)
+	cv2.imshow('SobelX', sobelX)
+	cv2.imshow('Sobel_Y', sobelY)
+	cv2.imshow('Sobel_Com', sobel_com)
 	cv2.imshow('Gray', gray)
 #image = [img,dst,blur,gblur,median,bilateral]
 	if cv2.waitKey(1) == ord('q'):
